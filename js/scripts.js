4406d1f7347416bc0043b4941db12d8f0d9d2eb4
Amr Elsagaei <amrelsagaei.social@gmail.com>
2022-01-31 05:26:37 +0300

    Education & Works Sections

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,26 @@
 Author: Amr Elsagaei                                                               */
 
 $(document).ready(function(){
-    $(window).scroll(function(){
-      if(this.scrollY > 20) 
-        $(".navbar").addClass("sticky");
-      else
-        $(".navbar").removeClass("sticky");
-    });
-  
-    $('.menu-toggler').click(function(){
-      $(this).toggleClass("active");
-      $(".navbar-menu").toggleClass("active");
-    });
+  $(window).scroll(function(){
+    if(this.scrollY > 20) 
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
   });
 
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+  
 /*                                                finish first part                   */
 /*                                                FINISH SECOND PART                  */
+
+  $(".works").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{enabled:true}
+  });
+/*                                                FINISH THIRD PART                  */
+});
+

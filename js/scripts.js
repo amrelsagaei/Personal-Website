a29d5b8831062e4880a609b66d9d021534d42522
Amr Elsagaei <amrelsagaei.social@gmail.com>
2022-02-03 06:14:22 +0300

    Contact & Footer

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,32 @@ Author: Amr Elsagaei                                                            
 
 $(document).ready(function(){
   $(window).scroll(function(){
-    if(this.scrollY > 20) 
+    if(this.scrollY > 20){
       $(".navbar").addClass("sticky");
-    else
+      $(".goTop").fadeIn();
+    }
+
+    else{
       $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
   });
+
+
+  $(".goTop").click(function() {scroll(0,0)});
 
   $('.menu-toggler').click(function(){
     $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
   
-/*                                                finish first part                   */
-/*                                                FINISH SECOND PART                  */
+
 
   $(".works").magnificPopup({
     delegate: 'a',
     type: 'image',
     gallery:{enabled:true}
   });
-/*                                                FINISH THIRD PART                  */
+  
 });
 
